955e91b6d9479aea123389aecc99837eeb4f6f57
Tjiaz <tunjiazeez24@gmail.com>
2025-02-03 21:42:29 +0000

    fixed terms page

--- src/app/terms/page.jsx
+++ src/app/terms/page.jsx
@@ -68,13 +68,13 @@ const Page = () => {
           <hr style={{ color: "#bbbbbb" }} />
           <div className={styles.termsContainer}>
             <p>
-              Please read these Terms of Service ("Terms", "Terms of Service")
-              carefully before using the www.azbytegems.com website (the
-              "Service") operated by Azbyte Gems ("us", "we", or "our"). Your
-              access to and use of the Service is conditioned upon your
-              acceptance of and compliance with these Terms. These Terms apply
-              to all visitors, users, and others who wish to access or use the
-              Service.
+              Please read these Terms of Service (&quot;Terms&quot;, &quot;Terms
+              of Service&quot;) carefully before using the www.azbytegems.com
+              website (the &quot;Service&quot;) operated by Azbyte Gems
+              (&quot;us&quot;, &quot;we&quot;, or &quot;our&quot;). Your access
+              to and use of the Service is conditioned upon your acceptance of
+              and compliance with these Terms. These Terms apply to all
+              visitors, users, and others who wish to access or use the Service.
             </p>
             <p>
               By accessing or using the Service, you agree to be bound by these
@@ -117,13 +117,13 @@ const Page = () => {
             <p>
               We may terminate or suspend access from certain IP addresses to
               the Service immediately, without prior notice or liability, under
-              our sole discretion, if we find excessive access to Azbyte Gems'
-              site or attempts to hack into our site or other security risks
-              from those IPs, or for other reasons, including but not limited to
-              a breach of the Terms. All provisions of the Terms which by their
-              nature should survive termination shall survive termination,
-              including, without limitation, ownership provisions, warranty
-              disclaimers, indemnity, and limitations of liability.
+              our sole discretion, if we find excessive access to Azbyte
+              Gems&apos; site or attempts to hack into our site or other
+              security risks from those IPs, or for other reasons, including but
+              not limited to a breach of the Terms. All provisions of the Terms
+              which by their nature should survive termination shall survive
+              termination, including, without limitation, ownership provisions,
+              warranty disclaimers, indemnity, and limitations of liability.
             </p>
             <h2>Indemnification</h2>
             <p>
@@ -131,7 +131,7 @@ const Page = () => {
               its licensee and licensors, and their employees, contractors,
               agents, officers, and directors, from and against any and all
               claims, damages, obligations, losses, liabilities, costs, or debt,
-              and expenses (including but not limited to attorney's fees),
+              and expenses (including but not limited to attorney&apos;ss fees),
               resulting from or arising out of a) your use and access of the
               Service, or b) a breach of these Terms.
             </p>
@@ -154,17 +154,17 @@ const Page = () => {
             <h2>Disclaimer</h2>
             <p>
               Your use of the Service is at your sole risk. The Service is
-              provided on an "AS IS" and "AS AVAILABLE" basis. The Service is
-              provided without warranties of any kind, whether express or
-              implied, including, but not limited to, implied warranties of
-              merchantability, fitness for a particular purpose,
-              non-infringement, or course of performance. Azbyte Gems, its
-              subsidiaries, affiliates, and its licensors do not warrant that a)
-              the Service will function uninterrupted, secure, or available at
-              any particular time or location; b) any errors or defects will be
-              corrected; c) the Service is free of viruses or other harmful
-              components; or d) the results of using the Service will meet your
-              requirements.
+              provided on an &quot;AS IS&quot; and &quot;AS
+              AVAILABLE&quot;basis. The Service is provided without warranties
+              of any kind, whether express or implied, including, but not
+              limited to, implied warranties of merchantability, fitness for a
+              particular purpose, non-infringement, or course of performance.
+              Azbyte Gems, its subsidiaries, affiliates, and its licensors do
+              not warrant that a) the Service will function uninterrupted,
+              secure, or available at any particular time or location; b) any
+              errors or defects will be corrected; c) the Service is free of
+              viruses or other harmful components; or d) the results of using
+              the Service will meet your requirements.
             </p>
             <h2>Exclusions</h2>
             <p>
@@ -189,12 +189,12 @@ const Page = () => {
             <p>
               We reserve the right, at our sole discretion, to modify or replace
               these Terms at any time. If a revision is material, we will
-              provide at least 30 days' notice prior to any new terms taking
-              effect. What constitutes a material change will be determined at
-              our sole discretion. By continuing to access or use our Service
-              after any revisions become effective, you agree to be bound by the
-              revised terms. If you do not agree to the new terms, you are no
-              longer authorized to use the Service.
+              provide at least 30 days&apos; notice prior to any new terms
+              taking effect. What constitutes a material change will be
+              determined at our sole discretion. By continuing to access or use
+              our Service after any revisions become effective, you agree to be
+              bound by the revised terms. If you do not agree to the new terms,
+              you are no longer authorized to use the Service.
             </p>
             <h2>Contact Us</h2>
             <p>
